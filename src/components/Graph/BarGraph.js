36a807ab2d854c90677490dffa86238ba976de08
David Sangojinmi <dsangojinmi26@gmail.com>
2021-03-03 13:42:07 +0000

    points appearing on graph

--- src/components/Graph/BarGraph.js
+++ src/components/Graph/BarGraph.js
@@ -1,22 +1,51 @@
-import React from "react";
+import React, { useState } from 'react';
 import Title from "./Title";
-import XAxis from "./XAxis";
-import YAxis from "./YAxis";
 
 const BarGraph = (props) => {
+    const graphHeight = 500;
+    const graphWidth = 800;
+    const numOfValues = props.values.length;
+    const [maxValue, setMaxValue] = useState({
+        value: 0
+    })
+    var i;
+    for (i = 0; i < numOfValues; i++) {
+        if (maxValue.value < props.values[i]) {
+            setMaxValue({
+                ...maxValue,
+                value: props.values[i]
+            })
+        }
+    }
+
     return (
         <>
             <Title title={props.title} />
-            <div>{props.values.map((i, n) => <div>Value is {i} and index is {n}</div>)}</div>
-            <svg>
+            <div>The max value is {maxValue.value}.</div>
+            <svg className="graph" height={graphHeight} width={graphWidth}>
+                <title>A line chart showing some information</title>
                 <g>
-                    {props.values.map((i, n) => <text x={((50 * n)).toString()} y="50">{n}</text>)}
                     <text x="400" y="450" className="line-graph-title">X-Axis</text>
                 </g>
+                <g class="grid grid-lines">
+                    <line x1="90" x2="90" y1="5" y2="371"></line>
+                </g>
+                <g class="grid grid-lines">
+                    <line x1="90" x2="705" y1="370" y2="370"></line>
+                </g>
+                <g>
+                    {props.values.map((value, index) =>
+                    <circle cx={(100 + ((584/(numOfValues - 1)) * index)).toString()} cy={(373 - ((358/maxValue.value) * value)).toString()} r="5" stroke="white" strokeWidth="5" fill="blue" />)}
+                </g>
+                <g className="labels x-labels">
+                    {props.values.map((value, index) =>
+                        <text x={(100 + ((584/(numOfValues - 1)) * index)).toString()} y="400">{index}</text>)}
+                </g>
+                <g className="labels y-labels">
+                    {(new Array(6)).fill(0).map((_, n) =>
+                        <text x="80" y={(15 + (71 * n)).toString()}>{maxValue.value - (maxValue.value/5) * n}</text>)}
+                </g>
             </svg>
-            <p>This will be a bar graph.</p>
-            <XAxis values={props.values} />
-            <YAxis values={props.values} />
         </>
     )
 }
