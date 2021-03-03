60bb788827d88864ecfdf0f45b8e486c614ea5f2
David Sangojinmi <dsangojinmi26@gmail.com>
2021-03-03 13:41:55 +0000

    not needed anymore

--- src/components/Graph/XAxis.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const XAxis = (props) => {
-    return (
-        <p>{props.values} This will be the XAxis.</p>   
-    )
-}
-
-export default XAxis;
--- src/components/Graph/YAxis.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const YAxis = (props) => {
-    return (
-        <p>This will be the YAxis.</p>   
-    )
-}
-
-export default YAxis;
