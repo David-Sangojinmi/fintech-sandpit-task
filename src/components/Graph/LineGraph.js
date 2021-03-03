887c8322f8943078a4ea4a6d98d250464ed45ba8
David Sangojinmi <dsangojinmi26@gmail.com>
2021-03-03 14:04:35 +0000

    points are now connected with lines

--- src/components/Graph/LineGraph.js
+++ src/components/Graph/LineGraph.js
@@ -1,59 +1,65 @@
-import React from "react";
+import React, { useState } from 'react';
 import Title from "./Title";
-import XAxis from "./XAxis";
-import YAxis from "./YAxis";
 
 const LineGraph = (props) => {
+    const graphHeight = 500;
+    const graphWidth = 800;
+    const numOfValues = props.values.length;
+    const [maxValue, setMaxValue] = useState({
+        value: 0
+    })
+    var i;
+    for (i = 0; i < numOfValues; i++) {
+        if (maxValue.value < props.values[i]) {
+            setMaxValue({
+                ...maxValue,
+                value: props.values[i]
+            })
+        }
+    }
+
     return (
         <>
             <Title title={props.title} />
-            <p>This will be a line graph.</p>
-            <svg className="graph">
-                <g className="grid x-grid" id="xGrid">
-                    <line x1="113" x2="113" y1="10" y2="380"></line>
-                    <line x1="259" x2="259" y1="10" y2="380"></line>
-                    <line x1="405" x2="405" y1="10" y2="380"></line>
-                    <line x1="551" x2="551" y1="10" y2="380"></line>
-                    <line x1="697" x2="697" y1="10" y2="380"></line>
+            <div>The max value is {maxValue.value}.</div>
+            <svg className="graph" height={graphHeight} width={graphWidth}>
+                <title>{props.title} line graph.</title>
+                <g>
+                    <text x="400" y="450" className="line-graph-title">X-Axis</text>
                 </g>
-                <g className="grid y-grid" id="yGrid">
-                    <line x1="86" x2="697" y1="10" y2="10"></line>
-                    <line x1="86" x2="697" y1="68" y2="68"></line>
-                    <line x1="86" x2="697" y1="126" y2="126"></line>
-                    <line x1="86" x2="697" y1="185" y2="185"></line>
-                    <line x1="86" x2="697" y1="243" y2="243"></line>
-                    <line x1="86" x2="697" y1="301" y2="301"></line>
-                    <line x1="86" x2="697" y1="360" y2="360"></line>
+                <g class="grid grid-lines">
+                    <line x1="90" x2="90" y1="5" y2="371"></line>
                 </g>
-                <g className="surfaces">
-                    <path className="first_set" d="M113,360 L113,192 L259,171 L405,179 L551,200 L697,204 L697,360 Z"></path>
+                <g class="grid grid-lines">
+                    <line x1="90" x2="705" y1="370" y2="370"></line>
                 </g>
-                <use className="grid double" ></use>
-                <use className="grid double" ></use>
-                <g className="first_set points" data-setname="Our first data set">
-                    <circle cx="113" cy="192" data-value="7.2" r="5"></circle>
-                    <circle cx="259" cy="171" data-value="8.1" r="5"></circle>
-                    <circle cx="405" cy="179" data-value="7.7" r="5"></circle>
-                    <circle cx="551" cy="200" data-value="6.8" r="5"></circle>
-                    <circle cx="697" cy="204" data-value="6.7" r="5"></circle>
+                <g class="grid grid-values">
+                    {props.values.map((value, index) =>
+                    <circle
+                        cx={(100 + ((584/(numOfValues - 1)) * index)).toString()}
+                        cy={(373 - ((358/maxValue.value) * value)).toString()}
+                        r="2"
+                        stroke="white"
+                        // strokeWidth="2"
+                        fill="red"
+                    />)}
+                    <polyline
+                        fill="none"
+                        stroke="white"
+                        strokeWidth="2"
+                        points={props.values.map((value, index) =>
+                            (100 + ((584/(numOfValues - 1)) * index)) + "," + (373 - ((358/maxValue.value) * value))).toString()}
+                    />
                 </g>
                 <g className="labels x-labels">
-                    <text x="113" y="400">2008</text>
-                    <text x="259" y="400">2009</text>
-                    <text x="405" y="400">2010</text>
-                    <text x="551" y="400">2011</text>
-                    <text x="697" y="400">2012</text>
+                    {props.values.map((value, index) =>
+                        <text x={(100 + ((584/(numOfValues - 1)) * index)).toString()} y="400">{index}</text>)}
                 </g>
                 <g className="labels y-labels">
-                    <text x="80" y="15">15</text>
-                    <text x="80" y="131">10</text>
-                    <text x="80" y="248">5</text>
-                    <text x="80" y="365">0</text>
-                    <text x="50" y="15">Weeks</text>
-                </g>    
+                    {(new Array(6)).fill(0).map((_, n) =>
+                        <text x="80" y={(15 + (71 * n)).toString()}>{maxValue.value - (maxValue.value/5) * n}</text>)}
+                </g>
             </svg>
-            <XAxis values={props.values} />
-            <YAxis values={props.values} />
         </>
     )
 }
